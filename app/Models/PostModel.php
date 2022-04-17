bde5fafe0c128d2ccfc72a6acbd589f685051208
ChimzDev <chimz.development@gmail.com>
2022-04-17 17:18:48 +0800

    Initial Commit
    1. Install Codeigniter 4 dengan Composer
    2. Setting Koneksi Database
    3. Migrasi Tabel Database Post
    4. Memuat Model Post
    5. Membuat Views Post
    6. Setting tampilan Pagination/ Pagers

---
+++ app/Models/PostModel.php
@@ -0,0 +1,18 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PostModel extends Model
+{
+    /**
+    * table name
+    */
+    protected $table = "posts";
+    /**
+    * allowed Field
+    */
+    protected $allowedFields = [
+        'judul_post',
+        'detail_post'
+    ];
+}
